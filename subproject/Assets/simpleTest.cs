09a0d2ba73f2272519d44a6b8d33653a8ad37051
shubham Prasad <shubham@totalcloud.io>
2018-11-16 11:03:25 +0530

    verifying if sub-projects can be tested in similar way

---
+++ subproject/Assets/simpleTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class SimpleTest {
+
+	[Test]
+	public void simpleTestSimplePasses() {
+		// Use the Assert class to test conditions.
+		Assert.AreEqual("okay", "okay");
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator simpleTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		Assert.AreEqual("ok", "ok");
+		yield return null;
+	}
+}
